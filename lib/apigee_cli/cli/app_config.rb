f52066e082c22a420aa82217dca0202ba4142474
Yumin Wong <yumin@bellycard.com>
2015-05-30 13:37:00 -0500

    some error handling for updating keyvaluemaps for a config_name

--- lib/apigee_cli/cli/app_config.rb
+++ lib/apigee_cli/cli/app_config.rb
@@ -7,10 +7,11 @@ class AppConfig < ThorCli
   desc 'pull', 'Pulls down keyvaluemaps from Apigee server'
   option :config_name, type: :string
   def pull
-    config_set = ApigeeCli::ConfigSet.new
+    config_set  = ApigeeCli::ConfigSet.new(environment)
+    config_name = options[:config_name]
 
-    if options[:config_name]
-      pull_entry(config_set, options[:config_name])
+    if config_name
+      pull_entry(config_set, config_name)
     else
       pull_list(config_set)
     end
@@ -19,9 +20,16 @@ class AppConfig < ThorCli
   desc 'push', 'Push up keyvaluemaps to Apigee server'
   option :config_name, type: :string
   def push(*configs)
+    config_set  = ApigeeCli::ConfigSet.new
     config_name = options[:config_name]
-    config_set = ApigeeCli::ConfigSet.new
-    orig_config_set = Hashie::Mash.new(config_set.read(config_name))
+
+    #TODO: compare orig_config_set with configs being passed in... provide option to overwrite...
+   #begin
+   #  orig_config_set = Hashie::Mash.new(config_set.read(config_name))
+   #rescue RuntimeError => e
+   #  render_error(e)
+   #  exit
+   #end
 
     data = []
     configs.each do |config|
@@ -30,9 +38,9 @@ class AppConfig < ThorCli
       data << { name: name, value: value }
     end
 
-    response = Hashie::Mash.new(config_set.update(config_name, data))
     changed_keys = data.map { |d| d[:name] }
-    render_entry(config_name, response['entry'], changed_keys)
+
+    update_entry(config_set, config_name, data, changed_keys)
   end
 
   private
@@ -40,9 +48,7 @@ class AppConfig < ThorCli
     def pull_list(config_set)
       response = Hashie::Mash.new(config_set.all)
       entries = response[MAP_KEY]
-      entries.each do |entry|
-        render_entry(entry['name'], entry['entry'])
-      end
+      render_list(entries)
     end
 
     def pull_entry(config_set, config_name)
@@ -54,8 +60,23 @@ class AppConfig < ThorCli
       end
     end
 
+    def update_entry(config_set, config_name, data, changed_keys)
+      begin
+        response = Hashie::Mash.new(config_set.update(config_name, data))
+        render_entry(config_name, response['entry'], changed_keys)
+      rescue RuntimeError => e
+        render_error(e)
+      end
+    end
+
+    def render_list(entries)
+      entries.each do |entry|
+        render_entry(entry['name'], entry['entry'])
+      end
+    end
+
     def render_entry(entry_name, key_values, highlight = [])
-      say("Config Name: #{entry_name}", :blue)
+      say("Environment: #{environment}, Config Name: #{entry_name}", :blue)
       key_values.each do |kv|
         name  = kv['name']
         value = kv['value']
